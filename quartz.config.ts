2a477c0f03e982d67599a955f8440e88f73c1877
losth230 <lafuethibaut@gmail.com>
2026-01-02 14:26:21 +0100

    Quartz sync: Jan 2, 2026, 2:26 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "BITES",
+    pageTitle: "C&P - JdR",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
