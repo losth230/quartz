286b1d75ebea9cef9ec5bf79b5d8c45d03798683
losth230 <lafuethibaut@gmail.com>
2026-01-02 12:06:26 +0100

    Quartz sync: Jan 2, 2026, 12:06 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,21 +1,21 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 
-// Components shared across the whole site
+// components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [], // Vous pouvez remettre les commentaires ici si vous voulez
+  afterBody : [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/losth230/quartz",
+      GitHub: "https://github.com/jackyzha0/quartz",
       "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }
 
-// Components on your main content pages
-export const defaultPage: PageLayout = {
+// pages that display a single page (e.g. a single note)
+export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
@@ -27,20 +27,17 @@ export const defaultPage: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    // L'explorateur (vos dossiers) est remonté ici
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    // La table des matières (Plan) est tout en haut
+    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
-    // Le graphique est tout en bas
-    Component.Graph(),
   ],
 }
 
-// Components for lists of pages (like tags or folders)
-export const listPage: PageLayout = {
+// pages that display lists of pages (e.g. tags or folders)
+export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
