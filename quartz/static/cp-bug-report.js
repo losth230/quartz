d4a56c4175d82fcbd57c0f80349676898b788383
losth230 <lafuethibaut@gmail.com>
2026-06-11 22:55:21 +0200

    Quartz sync: Jun 11, 2026, 10:55 PM

--- quartz/static/cp-bug-report.js
+++ quartz/static/cp-bug-report.js
@@ -8,7 +8,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 // ⬇️⬇️ REMPLACE CES DEUX VALEURS ⬇️⬇️
-const SUPABASE_URL = "https://TON-PROJET.kucgmmefluwmlobujanc.co";
+const SUPABASE_URL = "https://kucgmmefluwmlobujanc.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_YB_VCzZgD2vi4xeFvFT6ZA_BA9Pwn7R";
 // ⬆️⬆️ ----------------------------- ⬆️⬆️
 
