38b3c422a170416ced0d4e9a9ebf236edcb25d8c
losth230 <100137613+losth230@users.noreply.github.com>
2026-06-19 15:04:15 +0200

    Add cp-intro-gen.js script to Head component

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -102,6 +102,7 @@ export default (() => {
       <script type="module" src="/quartz/static/cp-army-lists.js" spa-preserve />
       <script type="module" src="/quartz/static/cp-bug-tracker.js" spa-preserve />
       <script type="module" src="/quartz/static/cp-stats.js" spa-preserve />
+      <script type="module" src="/quartz/static/cp-intro-gen.js" spa-preserve />
       <link rel="stylesheet" href="/quartz/static/cp-bug-report.css" spa-preserve />
       </head>
     )
