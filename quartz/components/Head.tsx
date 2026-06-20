14be061b5d85904a16385d109e3f018905ac7830
losth230 <100137613+losth230@users.noreply.github.com>
2026-06-20 19:39:08 +0200

    Add cp-saisie.css stylesheet to Head component

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -105,6 +105,7 @@ export default (() => {
       <script type="module" src="/quartz/static/cp-intro-gen.js" spa-preserve />
       <link rel="stylesheet" href="/quartz/static/cp-bug-report.css" spa-preserve />
       <link rel="stylesheet" href="/quartz/static/cp-intro-gen.css" spa-preserve />
+      <link rel="stylesheet" href="/quartz/static/cp-saisie.css" spa-preserve />
       </head>
     )
   }
